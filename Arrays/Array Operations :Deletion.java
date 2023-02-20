b6b6a936a950ac863d4bc9e627f7c395e862ffdb
Muhammad Adil Memon <96164867+muhammadadilmemon@users.noreply.github.com>
2023-02-20 16:25:29 +0500

    Create Array Operations :Deletion.java

---
+++ Arrays/Array Operations :Deletion.java
@@ -0,0 +1,37 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.*;
+public class Deletion {
+    public static void main(String[] args){
+        int n,m,p;
+        System.out.println("Enter the number of elements in array");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+       // System.out.println("Enter the number of elements in array");
+        int a[]=new int[n];
+        int b[]=new int[n-1];
+        System.out.println("Enter Values");
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        System.out.println("Enter the index value which you want to be Deleted: ");
+        m=sc.nextInt();
+        for(int i=0;i<a.length;i++){
+            if(i<m){
+                b[i]=a[i];
+            }
+            else if(i==m){
+                continue;
+            }
+            else {
+                b[i-1]=a[i];
+            }
+            System.out.println("Elements are: ");
+            for(i = 0; i<n-1; i++){
+                System.out.println(b[i]);
+            }
+
+        }
+
+    }
+}
